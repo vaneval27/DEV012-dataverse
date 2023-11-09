5f90ec2fcf461c9e91a36731467e36dc65f66e53
Vaneval <vaneval.dese@gmail.com>
2023-11-09 16:13:09 -0500

    update dataset with images. HTML structure

--- src/view.js
+++ src/view.js
@@ -1,6 +1,21 @@
 export const renderItems = (data) => {
   console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  const root = document.getElementById("root");
+
+  const ul = document.createElement("ul");
+  root.appendChild(ul);
+  ul.classList.add("character");
+  for (let i = 0; i < data.length; i++) {
+    ul.innerHTML += `<li itemscope itemtype= "card" class="cards">
+    
+    <div class="textCard">
+     <span><dt></dt><dd itemprop = "name">${data[i].name}</dd></span>
+     <img src="${data[i].imageUrl}" alt="img">
+     <span><dt></dt><dd itemprop = "occupation">${data[i].facts.occupation}</dd></span>
+     <span><dt>Affiliation: </dt><dd itemprop = "affiliation">${data[i].facts.affiliation}</dd></span>
+     <span><dt>Description: </dt><dd itemprop = "description">${data[i].description}</dd></span>      
+  </li> `;
+  }
+  return ul;
 };
 
